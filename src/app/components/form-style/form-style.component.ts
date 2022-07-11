e5859d23cb4c9ee7f0cbc7261af6b0f3dfaddef6
login <vheranin@gmail.com>
2022-07-11 17:39:46 +0300

    add UI main style

---
+++ src/app/components/form-style/form-style.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-style',
+  templateUrl: './form-style.component.html',
+  styleUrls: ['./form-style.component.css']
+})
+export class FormStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
